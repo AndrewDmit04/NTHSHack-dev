592ed2dd8d7b1be0c98ffb3d16e185149f056f9b
Shreya <shreya.singaravel@gmail.com>
2025-02-20 18:56:08 -0600

    feat: Change Video and graphics

--- components/homeComponents/HomeVideoStats.tsx
+++ components/homeComponents/HomeVideoStats.tsx
@@ -1,35 +1,50 @@
 import { stats } from '../../lib/data';
+import StarField from './StarField';
 
 export default function HomeVideoStats() {
   return (
-    <section className="z-10 relative md:h-[370px]">
-      <div className="flex flex-col justify-center items-center md:flex-row mx-auto py-[3rem]">
-        {/* Video */}
-        {/* !change */}
-        <div className="w-full aspect-video flex justify-center">
-          <iframe
-            className="w-7/8 md:w-[800px] md:h-[450px]"
-            src="https://www.youtube.com/embed/dMVtL2OmB60?si=ZKpc1VRAM6i-XmQQ"
-            title="YouTube video player"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowFullScreen
-          ></iframe>
-        </div>
+    <section className="z-0 relative py-[3rem] bg-gradient-to-r from-black to-blue-950 h-screen w-full">
+      {/* Video Section */}
+      <StarField count={100} />
+      <div className="w-full flex justify-center mr-10 translate-x-[-130px] translate-y-10 ">
+        <style jsx>
+          {`
+            @keyframes float {
+              0% {
+                transform: translateY(0);
+              }
+              50% {
+                transform: translateY(-20px);
+              }
+              100% {
+                transform: translateY(0);
+              }
+            }
 
-        {/* Stats */}
-        {/* <div>
-          {stats.map((stat, index) => (
-            <div
-              key={stat.data}
-              className={`${
-                index % 2 === 0 ? 'lg:ml-40 md:ml-20 ml-14' : 'md:mr-8 mr-24'
-              } text-center md:my-6 my-4`}
-            >
-              <p className="font-bold text-2xl text-primaryDark lg:text-5xl">{stat.data}</p>
-              <p className="font-medium text-lg lg:text-3xl">{stat.object}</p>
-            </div>
-          ))}
-        </div> */}
+            .animate-float {
+              animation: float 3s ease-in-out infinite;
+            }
+          `}
+        </style>
+        <img src="/assets/Moon.png" className="absolute bottom-0 right-5 w-80 h-80" />
+        <img
+          src="/assets/Astronaut.png"
+          className="absolute top-2 right-20 w-30 h-40 animate-float"
+        />
+        <img
+          src="/assets/spaceStation.png"
+          className="absolute bottom-10 left-40 w-50 h-50 animate-float"
+        />
+
+        <iframe
+          className="border-0 rounded-lg"
+          width="700"
+          height="400"
+          src="https://www.youtube.com/embed/Yf6X96oeNSs"
+          title="YouTube video player"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        ></iframe>
       </div>
     </section>
   );
